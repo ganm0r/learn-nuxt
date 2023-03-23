deb2a05e6b56c9fa52227d43e86eba6cfa745f41
ganm0r <gndhrvmv@gmail.com>
2023-03-23 16:36:57 +0530

    :construction: Build default layout

---
+++ layouts/default.vue
@@ -0,0 +1,50 @@
+<script setup>
+const links = [
+  {
+    name: "Products",
+    path: "/products",
+    exact: false,
+  },
+  {
+    name: "About",
+    path: "/about",
+    exact: true,
+  },
+];
+</script>
+
+<template>
+  <div class="w-screen">
+    <nav class="bg-zinc-200 flex gap-4 p-4 items-center">
+      <h1
+        class="font-bold text-2xl uppercase"
+        role="button"
+        @click="() => useRouter().replace('/')"
+      >
+        Nuxt Store
+      </h1>
+
+      <div
+        v-for="link in links"
+        class="flex h-full items-center"
+        :key="link.name"
+      >
+        <NuxtLink :exact="link.exact" :to="link.path" class="text-md font-semibold uppercase">{{
+          link.name
+        }}</NuxtLink>
+      </div>
+    </nav>
+
+    <div class="bg-stone-700">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.active-link {
+  text-decoration: underline;
+  text-underline-offset: 4px;
+  text-decoration-thickness: 2px;
+}
+</style>
