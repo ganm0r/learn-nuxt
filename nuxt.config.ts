0df23512ed60e2428783157432630e027def179d
ganm0r <gndhrvmv@gmail.com>
2023-03-23 16:36:32 +0530

    :lipstick: Add and set up Tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  router: {
+    options: {
+      linkActiveClass: "active-link",
+      linkExactActiveClass: "exact-active-link",
+    },
+  },
+});
